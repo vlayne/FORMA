5daea154aac6a963ac1d9185cfdfe1eb24dd4410
Rhoye <martin.romain81@gmail.com>
2015-12-10 10:06:33 +0100

    Modif10:06

---
+++ controleurs/c_inscrireFormation.php
@@ -0,0 +1,8 @@
+<?php
+$Numformation = $_REQUEST['formation'];
+$Numdomaine = $_REQUEST['domaine'];
+$NumSession = $_REQUEST['session'];
+var_dump($Numformation);
+var_dump($Numdomaine);
+var_dump($NumSession);
+?>
--- util/class.pdoForma.inc.php
+++ util/class.pdoForma.inc.php
@@ -201,7 +201,7 @@ class PdoForma
 	public function getLesFormations($idDom)
 	{
 		
-		$req = "select f.NOM_FORM, f.COUT_FORM, f.NBPLACE_FORM, f.LIEU_FORM, s.JOUR_SESSION, s.HEUREDEBUT_SESSIOn, s.HEUREFIN_SESSION from formation as f, session as s
+		$req = "select s.id_domaine,s.num_form,s.id_session,f.NOM_FORM, f.COUT_FORM, f.NBPLACE_FORM, f.LIEU_FORM, s.JOUR_SESSION, s.HEUREDEBUT_SESSIOn, s.HEUREFIN_SESSION from formation as f, session as s
 		where s.NUM_FORM = f.NUM_FORM and f.ID_DOMAINE = $idDom ";			
 		$res = PdoForma::$monPdo->query($req);
 		$lesLignes = $res->fetchAll();		
@@ -215,11 +215,6 @@ class PdoForma
 		$res = PdoForma::$monPdo->query($req);
 		$lesLignes = $res->fetchAll();
 		return $lesLignes;
-	}
-
-	public function getNbPlacesRestantes()
-	{
-		
 	}
 
 
